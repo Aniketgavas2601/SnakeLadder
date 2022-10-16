f7d37938575fe053061adf76f01e83d493e644d1
Experiment2601 <aniketgavas18@gmail.com>
2022-10-16 12:45:53 +0530

    Snake_And_Ladder_UC2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,13 +2,16 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
 
-    static void isPlayerPosition(){
-        byte playerPosition = 0;
-        System.out.println("Position of The Player at the time of Start the game: "+playerPosition);
+    static void getDiceRoll(){
+        int diceroll = 0;
+        System.out.println("Start the Game");
+        diceroll = (int) (Math.random() * 6 + 1);
+        System.out.println("Dice Number: "+diceroll);
+
     }
 
     public static void main(String[] args) {
-        SnakeAndLadder ref = new SnakeAndLadder();
-        ref.isPlayerPosition();
+        SnakeAndLadder dice = new SnakeAndLadder();
+        dice.getDiceRoll();
     }
 }
