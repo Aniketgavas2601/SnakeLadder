38eef863aadb05c86fcdb25ae188b4180f55e57a
Experiment2601 <aniketgavas18@gmail.com>
2022-10-16 12:53:39 +0530

    Snake_And_Ladder

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,12 +2,14 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
 
-    static void getWinner(){
-        int diceRoll = 0;
-        int snake[] = {30,37,57,78,99};
-        int ladder[] = {3,11,35,58,74};
+    static void getWinner() {
 
-        for (int playerPosition = 1; playerPosition <= 100; playerPosition++) {
+        int playerPosition = 0;
+        int diceRoll = 0;
+        int snake[] = {30, 37, 57, 78, 99};
+        int ladder[] = {3, 11, 35, 58, 74};
+
+        for ( playerPosition = 1; playerPosition <= 100; playerPosition++) {
             diceRoll = (int) (Math.random() * 6);
 
             System.out.println("Dice Number :" + " " + diceRoll);
@@ -15,95 +17,96 @@ public class SnakeAndLadder {
             playerPosition = playerPosition + diceRoll;
             playerPosition = playerPosition - 1;
 
-            System.out.println("Current Position of Player :"+playerPosition);
+            System.out.println("Current Position of Player :" + playerPosition);
 
             if (playerPosition == 100) {
                 System.out.println("CONGRATULATIONS!! YOU WON THE GAME.");
             }
 
-            if(diceRoll == 0){
+            if (diceRoll == 0) {
 
-                playerPosition = playerPosition -diceRoll;
+                playerPosition = playerPosition - diceRoll;
 
                 System.out.println("Please Roll a Die");
             }
 
-            if(playerPosition == snake[0]){
+            if (playerPosition == snake[0]) {
                 playerPosition = 10;
 
                 System.out.println("Its a Snake.");
                 System.out.println("you are Bitten By Snake.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == snake[1]){
+            } else if (playerPosition == snake[1]) {
                 playerPosition = 16;
 
                 System.out.println("Its a Snake.");
                 System.out.println("you are Bitten By Snake.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == snake[2]){
+            } else if (playerPosition == snake[2]) {
                 playerPosition = 32;
 
                 System.out.println("Its a Snake.");
                 System.out.println("you are Bitten By Snake.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == snake[3]){
+            } else if (playerPosition == snake[3]) {
                 playerPosition = 38;
 
                 System.out.println("Its a Snake.");
                 System.out.println("you are Bitten By Snake.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == snake[4]){
+            } else if (playerPosition == snake[4]) {
                 playerPosition = 12;
 
                 System.out.println("Its a Snake.");
                 System.out.println("you are Bitten By Snake.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == ladder[0]){
+            } else if (playerPosition == ladder[0]) {
                 playerPosition = 23;
 
                 System.out.println("Its a Ladder.");
                 System.out.println("you got a Ladder.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == ladder[1]){
+            } else if (playerPosition == ladder[1]) {
                 playerPosition = 45;
 
                 System.out.println("Its a Ladder.");
                 System.out.println("you got a Ladder.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == ladder[2]){
+            } else if (playerPosition == ladder[2]) {
                 playerPosition = 54;
 
                 System.out.println("Its a Ladder.");
                 System.out.println("you got a Ladder.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == ladder[3]){
-                playerPosition =85;
+            } else if (playerPosition == ladder[3]) {
+                playerPosition = 85;
 
                 System.out.println("Its a Ladder.");
                 System.out.println("you got a Ladder.");
-                System.out.println("You are at "+playerPosition+ " position.");
+                System.out.println("You are at " + playerPosition + " position.");
 
-            }else if(playerPosition == ladder[4]){
+            } else if (playerPosition == ladder[4]) {
                 playerPosition = 96;
 
                 System.out.println("Its a Ladder.");
                 System.out.println("you got a Ladder.");
-                System.out.println("You are at "+playerPosition+ " position.");
-            }else if (playerPosition > 100) {
+                System.out.println("You are at " + playerPosition + " position.");
+            } else if (playerPosition > 100) {
                 playerPosition = playerPosition - diceRoll;
 
                 System.out.println("YOU CAN'T JUMP, YOU MUST LAND ON A 100.");
                 System.out.println("You are at same position till the Exact Number Not get.");
             }
         }
+        System.out.println("Number of Times The Dice was rolled to Win the Game =" + diceRoll);
     }
 
     public static void main(String[] args) {
